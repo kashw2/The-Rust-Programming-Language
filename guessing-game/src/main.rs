cdee48c509bd8741858fd4d5cd9e34d798533060
kashw2 <supra4keanu@hotmail.com>
2024-01-13 11:39:38 +1000

    feat(guessing-game): updated with `match` statement

--- guessing-game/src/main.rs
+++ guessing-game/src/main.rs
@@ -1,15 +1,36 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("Guessing game!");
 
-    println!("Please input your number");
+    let secret_number = rand::thread_rng().gen_range(0..=100);
 
-    let mut input = String::new();
+    loop {
+        println!("Please input your number");
 
-    io::stdin()
-        .read_line(&mut input)
-        .expect("Error reading user input");
+        let mut input = String::new();
 
-    println!("Your guess was {input}");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Error reading user input");
+
+        let input: u32 = match input.trim().parse() {
+            Ok(number) => {
+                println!("Your guess was {number}");
+                number
+            },
+            Err(_) => continue
+        };
+
+        match input.cmp(&secret_number) {
+            Ordering::Less => println!("Your guess of {input} was too low"),
+            Ordering::Greater => println!("Your guess of {input} was too high"),
+            Ordering::Equal => {
+                println!("Your guess of {input} was right");
+                break;
+            }
+        }
+    }
 }
