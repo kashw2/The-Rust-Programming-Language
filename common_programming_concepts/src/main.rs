c4bd65ae4c357174fa389698465f41a40013b085
kashw2 <supra4keanu@hotmail.com>
2024-01-13 19:20:35 +1000

    feat(common-programming-concepts): chapter 3.2

--- common_programming_concepts/src/main.rs
+++ common_programming_concepts/src/main.rs
@@ -1,4 +1,7 @@
+use std::env::var;
+
 fn main() {
+    // Chapter 3.1
     let mut x: i32 = 10;
     println!("The value of x is {x}");
     x = 20;
@@ -10,4 +13,22 @@ fn main() {
     }
 
     println!("The final value of x is {x}");
+
+    // Chapter 3.2
+    let _u8: u8 = 255;
+    let _i8: i8 = -128;
+    let _u16: u16 = 65535;
+    let _i32: i32 = 999999999;
+    let _f64: f64 = 9.51923;
+
+    let tuple: (u8, f32, u16) = (123, 42.42, 9001);
+
+    let (_a, _b, _c) = tuple;
+
+    let _one_two_three = tuple.0;
+    let _meaning_of_life = tuple.1;
+    let _over_9000 = tuple.2;
+
+    let _days: [&str; 7] = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+    let _monday: &str = _days[0];
 }
